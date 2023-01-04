27ec47913c6a96fce9d878006be81d0d9e5cc9be
Khirby Calma <95603154+KhirbyCalma@users.noreply.github.com>
2023-01-03 16:16:57 -0800

    Reimplement result text output

--- script.js
+++ script.js
@@ -34,40 +34,57 @@ function updateScores(){
 function updateResult(playerMove, computerMove){
     if (playerMove === computerMove){
         resultText.textContent = `You both tie since you both chose ${playerMove}. Try again.`;
+        resultText.style.color = '#FEC83D';
     }
     else if ((playerMove === "ROCK" && computerMove === "PAPER") || 
             (playerMove === "PAPER" && computerMove === "SCISSOR") ||
             (playerMove === "SCISSOR" && computerMove === "ROCK")){
-        resultText.textContent = `You lose. ${computerMove} beats ${playerMove}.`;
+        if (isGameOver()){
+            resultText.textContent = `You lose. ${computerMove} beats ${playerMove}. Select a move to restart the game! First to 5 wins.`;
+            resultText.style.color = 'red';
+            resetGame();
+        }
+        else{
+            resultText.textContent = `You lose. ${computerMove} beats ${playerMove}.`;
+            resultText.style.color = 'red';
+        }
     }
     else if ((playerMove === "ROCK" && computerMove === "SCISSOR") || 
     (playerMove === "PAPER" && computerMove === "ROCK") ||
     (playerMove === "SCISSOR" && computerMove === "PAPER")){
-        resultText.textContent = `You win! ${playerMove} beats ${computerMove}.`;
+        if (isGameOver()){
+            resultText.textContent = `You win! ${playerMove} beats ${computerMove}. Select a move to restart the game! First to 5 wins.`;
+            resultText.style.color = 'green';
+            resetGame();
+        }
+        else{
+            resultText.textContent = `You win! ${playerMove} beats ${computerMove}.`;
+            resultText.style.color = 'green';
+        }
     }
 }
 
 function updateMoves(playerMove, computerMove){
     switch(playerMove){
         case "ROCK":
-            playerMoveText.textContent = `Your move: ${String.fromCodePoint(0x270A)}`
+            playerMoveText.textContent = `Your move: ${String.fromCodePoint(0x270A)}`;
             break;
         case "PAPER":
-            playerMoveText.textContent = `Your move: ${String.fromCodePoint(0x270B)}`
+            playerMoveText.textContent = `Your move: ${String.fromCodePoint(0x270B)}`;
             break;
         case "SCISSOR":
-            playerMoveText.textContent = `Your move: ${String.fromCodePoint(0x270C)}`
+            playerMoveText.textContent = `Your move: ${String.fromCodePoint(0x270C)}`;
             break;
     }
     switch(computerMove){
         case "ROCK":
-            computerMoveText.textContent = `Computer's move: ${String.fromCodePoint(0x270A)}`
+            computerMoveText.textContent = `Computer's move: ${String.fromCodePoint(0x270A)}`;
             break;
         case "PAPER":
-            computerMoveText.textContent = `Computer's move: ${String.fromCodePoint(0x270B)}`
+            computerMoveText.textContent = `Computer's move: ${String.fromCodePoint(0x270B)}`;
             break;
         case "SCISSOR":
-            computerMoveText.textContent = `Computer's move: ${String.fromCodePoint(0x270C)}`
+            computerMoveText.textContent = `Computer's move: ${String.fromCodePoint(0x270C)}`;
             break;
     }
 }
@@ -76,16 +93,12 @@ function isGameOver(){
     return (playerScore === 5 || computerScore === 5);
 }
 
-function checkWinner(){
-    if (playerScore === 5){
-        return "You win!";
-    }
-    return "Computer wins.";
-}
-
 function resetGame(){
     playerScore = 0;
     computerScore = 0;
+    updateScores();
+    playerMoveText.textContent = `Your move: ${String.fromCodePoint(0x2753)}`;
+    computerMoveText.textContent = `Computer's move: ${String.fromCodePoint(0x2753)}`;
 }
 
 function playRound(playerSelection, computerSelection){
@@ -108,24 +121,12 @@ function playRound(playerSelection, computerSelection){
 
 rockButton.addEventListener('click', () => {
     playRound("ROCK", getComputerChoice());
-    if (isGameOver()){
-        resultText.textContent = checkWinner();
-        resetGame();
-    }
 });
 
 paperButton.addEventListener('click', () => {
     playRound("PAPER", getComputerChoice());
-    if (isGameOver()){
-        resultText.textContent = checkWinner();
-        resetGame();
-    }
 });
 
 scissorButton.addEventListener('click', () => {
     playRound("SCISSOR", getComputerChoice());
-    if (isGameOver()){
-        resultText.textContent = checkWinner();
-        resetGame();
-    }
 });
