5f3d1cc2fdd7c3f1ea01b47cff56c9d937471f1b
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-18 12:59:32 -0700

    Add pseudocode for computer choice algorithm

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+// choose random # b/w 0 and 2, inclusively, and store in variable
+// if 0, return rock
+// if 1, return paper,
+// if 2, return scissors
+
+// return equation for random integer within specified range, inclusively
