f351b61aa669ff57e3e5d446e5fab55e97cb1aea
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-18 13:14:11 -0700

    Convert computer choice pseudocode to code with test

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-// choose random # b/w 0 and 2, inclusively, and store in variable
-// if 0, return rock
-// if 1, return paper,
-// if 2, return scissors
+function getComputerChoice() {
+    // choose random # b/w 0 and 2, inclusively, and store in variable
+    let randomComputerChoice = getRandomInteger(0, 2);
+    switch (randomComputerChoice) {
+        // if 0, return rock
+        case 0:
+            return 'ROCK';
+        // if 1, return paper,
+        case 1:
+            return 'PAPER';
+        // if 2, return scissors
+        case 2: 
+            return 'SCISSOR';
+        default:
+            return 'ERROR';
+    }
+}
 
-// return equation for random integer within specified range, inclusively
+function getRandomInteger(min, max) {
+    // return equation for random integer within specified range, inclusively
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// test to see if all choices are outputted by computer choice
+for (let i = 1; i <= 10; i++) {
+    console.log(`${i}. ${getComputerChoice()}`);
+}
